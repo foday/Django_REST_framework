9c404cef7b031965a898309a0c03e8a4958a161f
foday <foday_s@yahoo.com>
2020-12-25 15:40:28 -0600

    GROUP B-Step1: main urls.py and languages/urls.py

---
+++ api_example/languages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('languages', views.LanguageView)
+
+urlpatterns = [
+    path('', include(router.urls))
+    
+]
